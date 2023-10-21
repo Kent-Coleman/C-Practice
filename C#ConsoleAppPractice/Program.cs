67eae4d68cb802ec694896a01a7a0ac1c2cae02c
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-21 11:56:43 -0400

    More playing with built-in methods

--- C#ConsoleAppPractice/Program.cs
+++ C#ConsoleAppPractice/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Title = "Skynet";
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Hello, what's your name?");
+string userName = Console.ReadLine(); // Warning about a potential null entry
+Console.WriteLine("Nice to meet you, " + userName);
+Console.WriteLine("What's your favorite color?");
+string favColor = Console.ReadLine(); 
+Console.WriteLine(favColor + " is a great color!");
+
+Console.WriteLine("Press any key to continue...");
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+Console.WriteLine($"You pressed the key: {keyInfo.Key}");
